c4ffed2a47972d03544e1b95096c754e897943a7
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-08-14 16:35:34 +0530

    initialize server class

--- Chat-Application-Using-Socket-Programming/src/controller/server.java
+++ Chat-Application-Using-Socket-Programming/src/controller/server.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
 /**
  * created by kasunrachintha on 8/14/22
  *
@@ -8,4 +13,27 @@ package controller;
  * @Project : Chat-Application-Using-Socket-Programming
  **/
 public class server {
+
+    private static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(10001);
+        Socket accept;
+
+        while (true) {
+            System.out.println("waiting for client");
+            accept = serverSocket.accept();
+            System.out.println("new member connected!");
+            ClientHandler clientHandler = new ClientHandler(accept, clientHandlers);
+            clientHandlers.add(clientHandler);
+            clientHandler.start();
+
+        }
+
+        //  server server = new server(serverSocket);
+        //  server.startServer();
+
+
+    }
 }
