621b61142bee99d97c8f177e72a8b48c1632694a
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-08-14 16:37:01 +0530

    initialize loggin form

--- Chat-Application-Using-Socket-Programming/src/controller/LoginFromController.java
+++ Chat-Application-Using-Socket-Programming/src/controller/LoginFromController.java
@@ -2,6 +2,11 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * created by kasunrachintha on 8/14/22
@@ -13,6 +18,18 @@ import javafx.event.ActionEvent;
 public class LoginFromController {
     public JFXTextField txtUserName;
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public static String userName;
+
+
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        userName =txtUserName.getText();
+        Stage stage = (Stage) txtUserName.getScene().getWindow();
+        stage.close();
+        Stage stage1 = new Stage();
+        stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/clientForm.fxml"))));
+        stage1.setTitle("Online Chat ");
+        stage1.setResizable(false);
+        stage1.centerOnScreen();
+        stage1.show();
     }
 }
