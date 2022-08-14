982e03ec04c34f77384c6ed38c8d71c23a961a98
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-08-14 16:30:52 +0530

    Initialize clientformcontroller with ui

--- Chat-Application-Using-Socket-Programming/src/AppInitializer.java
+++ Chat-Application-Using-Socket-Programming/src/AppInitializer.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -8,7 +13,12 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("view/loginFrom.fxml"));
+        Scene mainScene = new Scene(parent);
+        primaryStage.setTitle("Live Chat");
+        primaryStage.setScene(mainScene);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
--- Chat-Application-Using-Socket-Programming/src/controller/ClientFormController.java
+++ Chat-Application-Using-Socket-Programming/src/controller/ClientFormController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
 /**
  * created by kasunrachintha on 8/14/22
  *
@@ -8,4 +15,16 @@ package controller;
  * @Project : Chat-Application-Using-Socket-Programming
  **/
 public class ClientFormController {
+    public Label lblUser;
+    public JFXTextField txtTextField;
+    public VBox vBox;
+    public ImageView imgSendImages;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void sendImgClicked(MouseEvent mouseEvent) {
+
+    }
 }
