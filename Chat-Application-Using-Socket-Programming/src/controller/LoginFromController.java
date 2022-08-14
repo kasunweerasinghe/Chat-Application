8f5c9b060a821f40c9a103bb5a5c56b764332857
kasun <https://github.com/GDSE-59-60-Project-Zone/Validations.git>
2022-08-14 16:26:43 +0530

    Added UI and Conytroller Classes

---
+++ Chat-Application-Using-Socket-Programming/src/controller/LoginFromController.java
@@ -0,0 +1,11 @@
+package controller;
+
+/**
+ * created by kasunrachintha on 8/14/22
+ *
+ * @author : kasunrachintha
+ * @Date :  8/14/22
+ * @Project : Chat-Application-Using-Socket-Programming
+ **/
+public class LoginFromController {
+}
